c5ad00e3a9695be0fb2cee849822c9482b2300af
Oceane <oceane.mauroux@epfl.ch>
2024-04-15 14:41:53 +0200

    First plot

--- code2.py
+++ code2.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-url = "https://docs.google.com/spreadsheets/d/16pNcfR5_Rw4r4JDUJ-Vr5JXtDCX747Yd/edit?usp=drive_link&ouid=113537870160569752188&rtpof=true&sd=true"
-url_for_pandas = url.replace("/edit?usp=share_link", "/export?format=xlsx")
-df = pd.read_excel(url_for_pandas)
+#url = "https://docs.google.com/spreadsheets/d/16pNcfR5_Rw4r4JDUJ-Vr5JXtDCX747Yd/edit?usp=drive_link&ouid=113537870160569752188&rtpof=true&sd=true"
+#url_for_pandas = url.replace("/edit?usp=share_link", "/export?format=xlsx")
+#df = pd.read_excel(url_for_pandas)
 
 
 
+# Charger les données depuis le fichier Excel
+file_path = 'C:\Documents\EPFL\MA4\Projet_de_semestre/br_100_s1_1a_P-V 1V_1#1.xls'
+data = pd.read_excel(file_path, sheet_name='br_100_s1_1a_P-V 1V_1#1.xls') 
