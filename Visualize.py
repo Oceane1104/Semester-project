9a7e91be461e2a9695b739f2597679e99f7ce040
Oceane <oceane.mauroux@epfl.ch>
2024-05-12 12:30:51 +0200

    Plot_CV,IV,PV,PUND

--- Visualize.py
+++ Visualize.py
@@ -3,16 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import numpy as np
-from make_dataset import load_interim_data
-from plot_settings import TITLE
+#from make_dataset_V2 import load_interim_data
+#from plot_settings import TITLE, LABEL, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, PATH_OUTPUT, LABEL_EXP, LABEL_PLAC, LABEL_GEO
 
-def plot_data(data_list, output_type_folder, graph_type):
-    plt.figure(figsize=(25, 12.5))
+
+def plot_PV(data_list, graph_type):
+    from make_dataset_V2 import load_interim_data
+    from plot_settings import TITLE, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, PATH_OUTPUT, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE
+
+
+    plt.figure(figsize=SIZE_PLOTS)
     colors = ['blue', 'red', 'green', 'orange', 'purple', 'yellow', 'orange', 'cyan']  # Liste de couleurs pour les courbes
     
-    for i, chip_name in enumerate(data_list):
-        data = load_interim_data(chip_name, graph_type)
-        name, geometrical, placement, experience =chip_name.split("_") 
+    for i, file_name in enumerate(data_list):
+        data = load_interim_data(file_name, graph_type)
+        name, geometrical, placement, experience =file_name.split("_") 
         size = geometrical.split("-")[0]
         Size_num = int(size)
         area = (Size_num*10**(-6))**2
@@ -21,50 +26,114 @@ def plot_data(data_list, output_type_folder, graph_type):
         charge_mi = min(data['Charge'])
 
         diff_charge = (charge_ma + charge_mi)/2
-        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)/area, marker='o', label=f"{name} - {experience} - {placement} - {geometrical}", 
-                        color=colors[i-1], linewidth=0.8)
-    plt.title(f'Polarisation vs Voltage ({placement}, Size {size}, graphe {graph_type}, experience {experience})', fontsize=25)
-    plt.xlabel('Voltage', fontsize=25)
-    plt.ylabel('Polarisation', fontsize=25)
-    plt.grid(True)
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=25)
-    plt.tick_params(axis='both', labelsize=20)
-    # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
-    plt.savefig(os.path.join(output_type_folder, f"Polarisation_vs_Voltage_{placement}_{size}_{graph_type}_{experience}.png"))
-    plt.close()
+        label_name = name
+        if (LABEL_EXP):
+            label_name = label_name + "_" + experience
+        if (LABEL_PLAC):
+            label_name = label_name + "_" + placement
+        if(LABEL_GEO):
+            label_name = label_name + "_" + geometrical
 
-def plot_pump(data_list, output_type_folder, graph_type):
-    plt.figure(figsize=(25, 12.5))
+        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)/area, marker='o', label=f"{label_name}", 
+                        color=colors[i-1], linewidth=SIZE_LINE)
+    plt.title(f'Polarisation vs Voltage {TITLE}', fontsize=SIZE_TITLE)
+    plt.xlabel('Voltage', fontsize=SIZE_AXIS)
+    plt.ylabel('Polarisation', fontsize=SIZE_AXIS)
+    plt.grid(True)
+    plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
+    #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=SIZE_LABELS)
+    plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
+    # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
+    plt.savefig(os.path.join(PATH_OUTPUT, f"{graph_type}_{TITLE}.png"))
+    plt.show()
+
+def plot_pund(data_list, graph_type):
+    from make_dataset_V2 import load_interim_data
+    from plot_settings import TITLE, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, PATH_OUTPUT, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE
+
+    plt.figure(figsize=SIZE_PLOTS)
     colors = ['blue', 'red', 'green', 'orange', 'purple', 'yellow', 'orange', 'cyan']  # Liste de couleurs pour les courbes
-    for chip_name in enumerate(data_list):
-        data = load_interim_data(chip_name, graph_type)
-        name, size, placement, experience =chip_name.split("_") 
-        plt.plot(data['t'], data['I'], marker='o', label=f"{chip_name} - {graph_type}", 
-                        color=colors[i-1], linewidth=0.8)
-    plt.title(f'Current vs Time ({placement}, Size {size}, experience {graph_type}, Type {experience})', fontsize=25)
-    plt.xlabel('Time', fontsize=25)
-    plt.ylabel('Current', fontsize=25)
+    for i, file_name in enumerate(data_list):
+        data = load_interim_data(file_name, graph_type)
+        name, geometrical, placement, experience =file_name.split("_") 
+
+        label_name = name
+        if (LABEL_EXP):
+            label_name = label_name + "_" + experience
+        if (LABEL_PLAC):
+            label_name = label_name + "_" + placement
+        if(LABEL_GEO):
+            label_name = label_name + "_" + geometrical
+
+        plt.plot(data['t'], data['I'], marker='o', label=f"{label_name}", 
+                        color=colors[i-1], linewidth=SIZE_LINE)
+    plt.title(f'Current vs Time {TITLE}', fontsize=SIZE_TITLE)
+    plt.xlabel('Time', fontsize=SIZE_AXIS)
+    plt.ylabel('Current', fontsize=SIZE_AXIS)
     plt.grid(True)
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=25)
-    plt.tick_params(axis='both', labelsize=20)
+    plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
+    plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
     # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
-    plt.savefig(os.path.join(output_type_folder, f"Current_vs_Time_{placement}_{size}_{graph_type}_{experience}.png"))
-    plt.close()
+    plt.savefig(os.path.join(PATH_OUTPUT, f"{graph_type}_{TITLE}.png"))
+    plt.show()
 
 
-def plot_leakage(data_list, experience, placement, size, new_chip_list, output_type_folder, graph_type):
-    plt.figure(figsize=(25, 12.5))
+def plot_IV(data_list, graph_type):
+    from make_dataset_V2 import load_interim_data
+    from plot_settings import TITLE, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, PATH_OUTPUT, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE
+
+    plt.figure(figsize=SIZE_PLOTS)
     colors = ['blue', 'red', 'green', 'orange', 'purple', 'yellow', 'orange', 'cyan']  # Liste de couleurs pour les courbes
-    for i, chip_name in enumerate(new_chip_list):
-        data = data_list[i]
-        plt.plot(data['AV'], data['AI'], marker='o', label=f"{chip_name} - {graph_type}", 
-                        color=colors[i-1], linewidth=0.8)
-    plt.title(f'Current vs Voltage ({placement}, Size {size}, experience {graph_type}, Type {experience})', fontsize=25)
-    plt.xlabel('Time', fontsize=25)
-    plt.ylabel('Current', fontsize=25)
+    for i, file_name in enumerate(data_list):
+        data = load_interim_data(file_name, graph_type)
+        name, geometrical, placement, experience =file_name.split("_") 
+
+        label_name = name
+        if (LABEL_EXP):
+            label_name = label_name + "_" + experience
+        if (LABEL_PLAC):
+            label_name = label_name + "_" + placement
+        if(LABEL_GEO):
+            label_name = label_name + "_" + geometrical
+        plt.plot(data['AV'], data['AI'], marker='o', label=f"{label_name}", 
+                        color=colors[i-1], linewidth=SIZE_LINE)
+    plt.title(f'Current vs Voltage {TITLE}', fontsize=SIZE_TITLE)
+    plt.xlabel('Time', fontsize=SIZE_AXIS)
+    plt.ylabel('Current', fontsize=SIZE_AXIS)
     plt.grid(True)
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=25)
-    plt.tick_params(axis='both', labelsize=20)
+    plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
+    plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
     # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
-    plt.savefig(os.path.join(output_type_folder, f"Current_vs_voltage_{placement}_{size}_{graph_type}_{experience}.png"))
-    plt.close()
+    plt.savefig(os.path.join(PATH_OUTPUT, f"{graph_type}_{TITLE}.png"))
+    plt.show()
+
+def plot_CV(data_list, graph_type):
+    from make_dataset_V2 import load_interim_data
+    from plot_settings import TITLE, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, PATH_OUTPUT, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE
+
+    plt.figure(figsize=SIZE_PLOTS)
+    colors = ['blue', 'red', 'green', 'orange', 'purple', 'yellow', 'orange', 'cyan']  # Liste de couleurs pour les courbes
+    for i, file_name in enumerate(data_list):
+        data = load_interim_data(file_name, graph_type)
+        name, geometrical, placement, experience =file_name.split("_") 
+
+        label_name = name
+        if (LABEL_EXP):
+            label_name = label_name + "_" + experience
+        if (LABEL_PLAC):
+            label_name = label_name + "_" + placement
+        if(LABEL_GEO):
+            label_name = label_name + "_" + geometrical
+        plt.plot(data['DCV_AB'], data['Cp_AB'], marker='o', label=f"{label_name}", 
+                        color=colors[i-1], linewidth=SIZE_LINE)
+    plt.title(f'Charge vs Voltage {TITLE}', fontsize=SIZE_TITLE)
+    plt.xlabel('Charge', fontsize=SIZE_AXIS)
+    plt.ylabel('Voltage', fontsize=SIZE_AXIS)
+    plt.grid(True)
+    plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
+    plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
+    # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
+    plt.savefig(os.path.join(PATH_OUTPUT, f"{graph_type}_{TITLE}.png"))
+    plt.show()
+
+    
--- plot_settings.py
+++ plot_settings.py
@@ -12,6 +12,7 @@ from tools import select_capas_with_parameter
 from tools import extract_voltage_in_graphtype
 from tools import load_process_param_df
 from tools import load_geom_param_df
+from Visualize import plot_PV, plot_CV, plot_pund, plot_IV
 
 
 ## PATHS
@@ -39,17 +40,28 @@ PATH_PROCESS_PARAM_FILE = PATH_FOLDER + '\\User_input\\process_parameter.xlsx'
 PATH_GEOM_PARAM_FILE = PATH_FOLDER + '\\User_input\\geometrical_parameter.xlsx'
 
 # Load parameters
-param_df = load_process_param_df(PATH_PROCESS_PARAM_FILE)
-geom_df = load_geom_param_df(PATH_GEOM_PARAM_FILE)
+#param_df = load_process_param_df(PATH_PROCESS_PARAM_FILE)
+#geom_df = load_geom_param_df(PATH_GEOM_PARAM_FILE)
 
 
 # Plot settings
-TITLE = ""
+TITLE = "TEST"
+LABEL_EXP = True
+LABEL_PLAC = True
+LABEL_GEO = True
 LABEL = "" # experience? placement? geometrie? true/false
-SIZE_TITLE = 1
-SIZE_AXIS = 1
-SIZE_LABELS = 1
-SIZE_PLOTS = 1
+SIZE_TITLE = 25
+SIZE_AXIS = 25
+SIZE_LABELS = 25
+SIZE_GRADUATION = 20
+SIZE_PLOTS = (25, 12.5)
+SIZE_LINE = 0.8
+BOX_PLACE = (0.01, 0.95)
+LOC_PLACE = "upper left"
+
+#m, μ, n, p
+UNIT_X = m
+UNIT_Y = 1
 
 units = ""
 
@@ -69,3 +81,6 @@ VOLTAGES = ["1", "3", "","5"]
 RESULTS_TO_PLOT = []
 RESULTS_X_AXIS = "parameter x"
 RESULTS_LABELS = ""
+
+data_list = ["3dec09_50_bl-s3-c4_DP-450-120", "3dec09_100_bl-s2-4a_DP-450-120"]
+plot_CV(data_list, "CV 3V_1#1")
