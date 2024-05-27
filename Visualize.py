6d4bee4f4881f5203279100fa5e6d46aa8c4409c
nthlknzl <nathalie.kuenzle@bluewin.ch>
2024-05-27 12:27:09 +0200

    label names changed

--- Visualize.py
+++ Visualize.py
@@ -26,16 +26,46 @@ def load_interim_data(interim_file_name, graph_type, interim_path):
                 # Read the Excel file
                 data_df = pd.read_excel(file_path, sheet_name=graph_type)
             else:
-                print("Sheet name",  graph_type ,"inexistant for capacitor", interim_file_name)
+                print("WARNING: Sheet name",  graph_type ,"inexistant for capacitor", interim_file_name)
     else:
         print("ERROR: File",interim_file_name,"doesn't exist in path", file_path)
 
     return data_df
 
+def define_label_name(file_name, process_df, geom_df):
+    chip_name, geometrical, placement, experience = file_name.split("_") 
 
+    process_names = process_df.columns
+    geom_names = geom_df.columns
+
+    if len(process_names)>1:
+        exp_parts = experience.split("-")
+    else:
+        exp_parts = [experience]
+
+    if len(geom_names)>1:
+        geom_parts = geometrical.split("-")
+    else:
+        geom_parts = [geometrical]
+
+    # insert "_" in chip name
+    name_start = chip_name.split("4")[0]
+    name_end = chip_name.split(name_start)[1]
+    chip_name_new = name_start + "_" + name_end
+
+    label_name = "Chip: " + chip_name_new
+    if (LABEL_EXP):
+        for i in range(len(process_names)):
+            label_name = label_name + ", " + process_names[i] + ": " + exp_parts[i]
+    if(LABEL_GEO):
+        for i in range(len(geom_names)):
+            label_name = label_name + ", " + geom_names[i] + ": " + geom_parts[i]
+    if (LABEL_PLAC):
+        label_name = label_name + ", Placement: " + placement
+    return label_name
 
 #***** Plot functions *****
-def plot_PV(data_list, graph_type, interim_path, output_path):
+def plot_PV(data_list, graph_type, interim_path, output_path, process_df, geom_df):
     nb_plots = 0
 
     plt.figure(figsize=SIZE_PLOTS)
@@ -46,7 +76,7 @@ def plot_PV(data_list, graph_type, interim_path, output_path):
         if data.empty:
             continue
         nb_plots += 1
-        name, geometrical, placement, experience =file_name.split("_") 
+        geometrical = file_name.split("_")[1]
         size = geometrical.split("-")[0]
         Size_num = int(size)
         area = (Size_num*10**(-6))**2
@@ -55,13 +85,8 @@ def plot_PV(data_list, graph_type, interim_path, output_path):
         charge_mi = min(data['Charge'])
 
         diff_charge = (charge_ma + charge_mi)/2
-        label_name = name
-        if (LABEL_EXP):
-            label_name = label_name + "_" + experience
-        if (LABEL_PLAC):
-            label_name = label_name + "_" + placement
-        if(LABEL_GEO):
-            label_name = label_name + "_" + geometrical
+
+        label_name = define_label_name(file_name, process_df, geom_df)
 
         plt.plot(data['Vforce'], (data['Charge'] - diff_charge)/area, marker='o', label=f"{label_name}", 
                         color=colors[(i-1)%len(colors)], linewidth=SIZE_LINE)
@@ -70,7 +95,7 @@ def plot_PV(data_list, graph_type, interim_path, output_path):
         return
     plt.title(f"{graph_type} {TITLE}", fontsize=SIZE_TITLE)
     plt.xlabel('Voltage [V]', fontsize=SIZE_AXIS)
-    plt.ylabel('Polarisation [mC/cm2]', fontsize=SIZE_AXIS)
+    plt.ylabel('Polarisation [uC/cm2]', fontsize=SIZE_AXIS)
     plt.grid(True)
     plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
     #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=SIZE_LABELS)
@@ -81,7 +106,7 @@ def plot_PV(data_list, graph_type, interim_path, output_path):
         plt.show()
     return
 
-def plot_pund(data_list, graph_type, interim_path, output_path):
+def plot_pund(data_list, graph_type, interim_path, output_path, process_df, geom_df):
     nb_plots = 0
     plt.figure(figsize=SIZE_PLOTS)
     colors = COLORS  # Liste de couleurs pour les courbes
@@ -92,16 +117,8 @@ def plot_pund(data_list, graph_type, interim_path, output_path):
         nb_plots +=1
         
         filtered_I = butter_lowpass_filter(data['I'], data['t'])
-        name, geometrical, placement, experience =file_name.split("_") 
-
-        label_name = name
-        if (LABEL_EXP):
-            label_name = label_name + "_" + experience
-        if (LABEL_PLAC):
-            label_name = label_name + "_" + placement
-        if(LABEL_GEO):
-            label_name = label_name + "_" + geometrical
-
+    
+        label_name = define_label_name(file_name, process_df, geom_df)
         plt.plot(data['t'], filtered_I, marker='o', label=f"{label_name}", 
                         color=colors[(i-1)%len(colors)], linewidth=SIZE_LINE)
     if nb_plots == 0:
@@ -120,7 +137,7 @@ def plot_pund(data_list, graph_type, interim_path, output_path):
     return
 
 
-def plot_IV(data_list, graph_type, interim_path, output_path):
+def plot_IV(data_list, graph_type, interim_path, output_path, process_df, geom_df):
     nb_plots = 0
     plt.figure(figsize=SIZE_PLOTS)
     colors = COLORS  # Liste de couleurs pour les courbes
@@ -129,15 +146,8 @@ def plot_IV(data_list, graph_type, interim_path, output_path):
         if data.empty: 
             continue
         nb_plots +=1
-        name, geometrical, placement, experience =file_name.split("_") 
 
-        label_name = name
-        if (LABEL_EXP):
-            label_name = label_name + "_" + experience
-        if (LABEL_PLAC):
-            label_name = label_name + "_" + placement
-        if(LABEL_GEO):
-            label_name = label_name + "_" + geometrical
+        label_name = define_label_name(file_name, process_df, geom_df)
         plt.plot(data['AV'], data['AI'], marker='o', label=f"{label_name}", 
                         color=colors[i-1], linewidth=SIZE_LINE)
     if nb_plots == 0:
@@ -145,7 +155,7 @@ def plot_IV(data_list, graph_type, interim_path, output_path):
         return
     plt.title(f"{graph_type} {TITLE}", fontsize=SIZE_TITLE)
     plt.xlabel('Voltage [V]', fontsize=SIZE_AXIS)
-    plt.ylabel('Current [?]', fontsize=SIZE_AXIS)
+    plt.ylabel('Current [A]', fontsize=SIZE_AXIS)
     plt.grid(True)
     plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
     plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
@@ -155,7 +165,7 @@ def plot_IV(data_list, graph_type, interim_path, output_path):
         plt.show()
     return
 
-def plot_CV(data_list, graph_type, interim_path, output_path):
+def plot_CV(data_list, graph_type, interim_path, output_path, process_df, geom_df):
     nb_plots = 0
     plt.figure(figsize=SIZE_PLOTS)
     colors = COLORS  # Liste de couleurs pour les courbes
@@ -164,15 +174,8 @@ def plot_CV(data_list, graph_type, interim_path, output_path):
         if data.empty: 
             continue
         nb_plots +=1
-        name, geometrical, placement, experience =file_name.split("_") 
-
-        label_name = name
-        if (LABEL_EXP):
-            label_name = label_name + "_" + experience
-        if (LABEL_PLAC):
-            label_name = label_name + "_" + placement
-        if(LABEL_GEO):
-            label_name = label_name + "_" + geometrical
+        
+        label_name = define_label_name(file_name, process_df, geom_df)
         plt.plot(data['DCV_AB'], data['Cp_AB'], marker='o', label=f"{label_name}", 
                         color=colors[(i-1)%len(colors)], linewidth=SIZE_LINE)
     if nb_plots == 0:
