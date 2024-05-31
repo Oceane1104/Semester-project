85ea829c0eafb49a786c67c492b59df208ecafa3
Oceane <oceane.mauroux@epfl.ch>
2024-05-31 14:35:19 +0200

    Test_result

--- Visualize.py
+++ Visualize.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import numpy as np
+from scipy.integrate import simps
 
 from tools import butter_lowpass_filter, load_process_param_df, load_geom_param_df
 
-from plot_settings import TITLE, LABEL, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE, SHOW_PLOTS
+from plot_settings import TITLE, LABEL, SIZE_TITLE, SIZE_AXIS, SIZE_LABELS, SIZE_GRADUATION, SIZE_PLOTS, SIZE_LINE, LABEL_EXP, LABEL_PLAC, LABEL_GEO, BOX_PLACE, LOC_PLACE, SHOW_PLOTS, NAME, LABEL_GRAPH, LABEL_NB_EXP
+from plot_settings import TITLE_ADD_SIZE, TITLE_ADD_GRAPH
 
 COLORS = ['blue', 'red', 'green', 'orange', 'purple', 'yellow', 'orange', 'cyan', 'brown', 'gray', 'olive', 'pink']
 
@@ -32,6 +34,12 @@ def load_interim_data(interim_file_name, graph_type, interim_path):
 
     return data_df
 
+# Fonction pour extraire la partie souhaitée
+def extract_relevant_part(graph_type):
+    pattern = r"P-V \d+V"
+    match = re.search(pattern, graph_type)
+    return match.group(0) if match else graph_type
+
 def define_label_name(file_name, process_df, geom_df, graph_type = ""):
     chip_name, geometrical, placement, experience = file_name.split("_") 
 
@@ -53,15 +61,36 @@ def define_label_name(file_name, process_df, geom_df, graph_type = ""):
     name_end = chip_name.split(name_start)[1]
     chip_name_new = name_start + "_" + name_end
 
-    label_name = "Chip: " + chip_name_new 
-    if graph_type != "":
-        label_name = label_name + "Graph: "+ graph_type
+    label_name = ""
+
+    if(NAME):
+        label_name = "Chip: " + chip_name_new 
+
+    if (LABEL_GRAPH):
+        if graph_type != "":
+            if (label_name == ""):
+                label_name = "Graph: "+ extract_relevant_part(graph_type)
+            else:
+                label_name = label_name + ", Graph: "+ extract_relevant_part(graph_type)
     if (LABEL_EXP):
-        for i in range(len(process_names)):
-            label_name = label_name + ", " + process_names[i] + ": " + exp_parts[i]
+        if (LABEL_NB_EXP == []):
+            for i in range(len(process_names)):
+                if (label_name == ""):
+                    label_name = process_names[i] + ": " + exp_parts[i]
+                else:
+                    label_name = label_name + ", " + process_names[i] + ": " + exp_parts[i]
+        else:
+            for i in range(len(LABEL_NB_EXP)):
+                if (label_name == ""):
+                    label_name = process_names[LABEL_NB_EXP[i]] + ": " + exp_parts[LABEL_NB_EXP[i]]
+                else:
+                    label_name = label_name + ", " + process_names[LABEL_NB_EXP[i]] + ": " + exp_parts[LABEL_NB_EXP[i]]
     if(LABEL_GEO):
         for i in range(len(geom_names)):
-            label_name = label_name + ", " + geom_names[i] + ": " + geom_parts[i]
+            if (label_name == ""):
+                label_name = geom_names[i] + ": " + geom_parts[i] 
+            else:
+                label_name = label_name + ", " + geom_names[i] + ": " + geom_parts[i]
     if (LABEL_PLAC):
         label_name = label_name + ", Placement: " + placement
     return label_name
@@ -81,7 +110,7 @@ def plot_PV(data_list, graph_type, interim_path, output_path, process_df, geom_d
         geometrical = file_name.split("_")[1]
         size = geometrical.split("-")[0]
         Size_num = int(size)
-        area = (Size_num*10**(-6))**2
+        area = (Size_num*10**(-4))**2
         
         charge_ma = max(data['Charge'])
         charge_mi = min(data['Charge'])
@@ -90,20 +119,27 @@ def plot_PV(data_list, graph_type, interim_path, output_path, process_df, geom_d
 
         label_name = define_label_name(file_name, process_df, geom_df)
 
-        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)/area, marker='o', label=f"{label_name}", 
+        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)*10**6/area, marker='o', label=f"{label_name}", 
                         color=colors[(i-1)%len(colors)], linewidth=SIZE_LINE)
+
     if nb_plots == 0:
         print("No", graph_type, "data available for for capacitors", data_list)
         return
-    plt.title(f"{graph_type} {TITLE}", fontsize=SIZE_TITLE)
+    Finish_title = f"{TITLE}"
+    if (TITLE_ADD_GRAPH):
+        Finish_title = f"{Finish_title} for {extract_relevant_part(graph_type)}"
+    if (TITLE_ADD_SIZE):
+        Finish_title = f"{Finish_title} for {size}µm"
+
+    plt.title(Finish_title, fontsize=SIZE_TITLE)
     plt.xlabel('Voltage [V]', fontsize=SIZE_AXIS)
-    plt.ylabel('Polarisation [uC/cm2]', fontsize=SIZE_AXIS)
+    plt.ylabel('Polarisation [µC/cm2]', fontsize=SIZE_AXIS)
     plt.grid(True)
     plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
     #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=SIZE_LABELS)
     plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
     # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
-    plt.savefig(os.path.join(output_path, f"{graph_type}_{TITLE}.png"))
+    plt.savefig(os.path.join(output_path, f"{Finish_title}.png"))
     if SHOW_PLOTS:
         plt.show()
     return
@@ -209,7 +245,7 @@ def plot_PV_special(data_list, graph_type, interim_path, output_path, special_gr
         geometrical =file_name.split("_")[1]
         size = geometrical.split("-")[0]
         Size_num = int(size)
-        area = (Size_num*10**(-6))**2
+        area = (Size_num*10**(-4))**2
         
         charge_ma = max(data['Charge'])
         charge_mi = min(data['Charge'])
@@ -217,20 +253,143 @@ def plot_PV_special(data_list, graph_type, interim_path, output_path, special_gr
         diff_charge = (charge_ma + charge_mi)/2
 
         label_name = define_label_name(file_name, process_df, geom_df, graph_type[i])
-        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)/area, marker='o', label=f"{label_name}", 
+        plt.plot(data['Vforce'], (data['Charge'] - diff_charge)*10**6/area, marker='o', label=f"{label_name}", 
                         color=colors[(i-1)%len(colors)], linewidth=SIZE_LINE)
     if nb_plots == 0:
         print("No", graph_type, "data available for for capacitors", data_list)
         return
-    plt.title(f"{list(set(special_graph))} {TITLE}", fontsize=SIZE_TITLE)
+    #{list(set(special_graph))} 
+    plt.title(f"{TITLE}", fontsize=SIZE_TITLE)
     plt.xlabel('Voltage [V]', fontsize=SIZE_AXIS)
-    plt.ylabel('Polarisation [mC/cm2]', fontsize=SIZE_AXIS)
+    plt.ylabel('Polarisation [µC/cm2]', fontsize=SIZE_AXIS)
     plt.grid(True)
     plt.legend(loc=LOC_PLACE, bbox_to_anchor=BOX_PLACE, fontsize=SIZE_LABELS)
     #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=SIZE_LABELS)
     plt.tick_params(axis='both', labelsize=SIZE_GRADUATION)
     # Sauvegarder le plot dans le dossier spécifié avec le titre comme nom de fichier
-    plt.savefig(os.path.join(output_path, f"{list(set(special_graph))}_{TITLE}.png"))
+    plt.savefig(os.path.join(output_path, f"{TITLE}.png"))
     if SHOW_PLOTS:
         plt.show()
     return
+
+def plot_PV_and_calculate_energy(data_list, graph_type, interim_path, output_path, process_df, geom_df):
+    results = []
+    
+    plt.figure(figsize=(10, 6))
+    colors = plt.cm.viridis(np.linspace(0, 1, len(data_list)))
+    
+    for i, file_name in enumerate(data_list):
+        data = load_interim_data(file_name, graph_type, interim_path)
+        if data.empty:
+            continue
+        
+        geometrical = file_name.split("_")[1]
+        size = geometrical.split("-")[0]
+        Size_num = int(size)
+        area = (Size_num * 10**(-6)) ** 2
+        
+        charge_ma = max(data['Charge'])
+        charge_mi = min(data['Charge'])
+        diff_charge = (charge_ma + charge_mi) / 2
+        
+        Vforce = data['Vforce']
+        Polarisation = (data['Charge'] - diff_charge) / area
+        
+        # Find the index of the maximum voltage
+        max_voltage_idx = np.argmax(Vforce)
+
+        # Calculate energy densities
+        n = len(Vforce)
+        energy_total = simps(Vforce[:max_voltage_idx], Polarisation[:max_voltage_idx])
+        energy_density = -simps(Vforce[max_voltage_idx:n//2], Polarisation[max_voltage_idx:n//2])
+        energy_lost = energy_total - energy_density
+
+        print(f"L'energy density est {energy_density}")
+        print(f"L'energy perdue est {energy_lost}")
+
+        label_name = f"L'energy total: {energy_total} L'energy density: {energy_density} et L'energy perdue: {energy_lost}"
+        
+        # Plot PV curve
+        plt.plot(Vforce, Polarisation, marker='o', label=f"{label_name}", color=colors[i], linewidth=2)
+
+        results.append({
+            'File': file_name,
+            'Energy Total (uJ)': energy_total,
+            'Energy Density (uJ/cm^2)': energy_density,
+            'Energy Lost (uJ)': energy_lost
+        })
+        
+        # Shade areas under the curves
+        plt.fill_between(Vforce[:max_voltage_idx], Polarisation[:max_voltage_idx], color=colors[i], alpha=0.1)
+        plt.fill_between(Vforce[max_voltage_idx:n//2], Polarisation[max_voltage_idx:n//2], color=colors[i], alpha=0.3)
+
+        # Add red points for energy_total integration range
+        plt.scatter([Vforce[max_voltage_idx], Vforce[n//2]], [Polarisation[max_voltage_idx], Polarisation[n//2]], color='red', zorder=5)
+
+        # Add blue points for energy_density integration range
+        plt.scatter([Vforce[0], Vforce[max_voltage_idx]], [Polarisation[0], Polarisation[max_voltage_idx]], color='blue', zorder=5)
+    
+    if not results:
+        print("No", graph_type, "data available for capacitors", data_list)
+        return
+    
+    # Save results to Excel
+    results_df = pd.DataFrame(results)
+    results_df.to_excel(os.path.join(output_path, "energy_results.xlsx"), index=False)
+    
+    # Plot configuration
+    plt.title(f"{graph_type} PV Curves", fontsize=14)
+    plt.xlabel('Voltage [V]', fontsize=12)
+    plt.ylabel('Polarisation [µC/cm²]', fontsize=12)
+    plt.grid(True)
+    plt.legend(loc='best', fontsize=10)
+    plt.savefig(os.path.join(output_path, f"{graph_type}_PV_plot.png"))
+    plt.show()
+
+    return results_df
+
+#Result of charge energy
+def load_process_parameters(file_path):
+    return pd.read_excel(file_path)
+
+def load_results(file_path):
+    return pd.read_excel(file_path)
+
+def extract_energy_data(process_df, data_list, result_folder):
+    results = []
+    for chip in data_list:
+        # Trouvez l'index de la ligne correspondant à la chip spécifique dans la première colonne
+        chip_index = process_df.index[process_df['sample ID'] == chip]
+
+        if len(chip_index) == 0:
+            print(f"Chip {chip} n'a pas été trouvé dans le DataFrame.")
+        else:
+            # Récupérez la ligne complète correspondant à la chip
+            row = process_df.loc[chip_index[0]]
+
+            # Générer le nom de fichier en joignant les valeurs de la ligne avec des tirets
+            file_name = "-".join(row.iloc[1:].astype(str)) + f"_{chip}.xlsx"
+            print(f"Ceci est {file_name}")
+            file_path = os.path.join(result_folder, file_name)
+
+            if not os.path.exists(file_path):
+                print(f"Le fichier {file_path} n'existe pas.")
+            else:
+                # Chargez les résultats si le fichier existe
+                data = load_results(file_path)
+                # Vous pouvez maintenant travailler avec les données chargées
+        for col in data.columns:
+                if col.startswith("Energy density"):
+                    voltage = int(col.split()[2].replace('V_1', ''))
+                    energy_density_mean = data[col][data['Chip'] == 'MEA'].values[0]
+                    energy_total_col = col.replace("Energy density", "Energy total")
+                    energy_total_mean = data[energy_total_col][data['Chip'] == 'MEA'].values[0]
+                    efficiency = (energy_density_mean / energy_total_mean) * 100
+
+                    results.append({
+                        'Chip': chip,
+                        'Voltage': voltage,
+                        'Energy_density': energy_density_mean,
+                        'Efficiency': efficiency
+                    })
+    return pd.DataFrame(results)
--- plot_settings.py
+++ plot_settings.py
@@ -1,32 +1,43 @@
 # Plot settings
-TITLE = "Compare PUND vs PV"
+TITLE = "TEST"
+FOLDER = "TEST"
+#Evolution of DE1FE5x8 with 450°C from 5V to 30V for 200µm capacitor
+
+TITLE_ADD_SIZE = True
+TITLE_ADD_GRAPH = True
+
+NAME = False
 LABEL_EXP = True
-LABEL_NB_EXP = [0, 1]
+LABEL_NB_EXP = []
 LABEL_PLAC = True
 LABEL_GEO = True
+LABEL_GRAPH = False
 LABEL = "" # experience? placement? geometrie? true/false
 SIZE_TITLE = 25
 SIZE_AXIS = 25
 SIZE_GRADUATION = 20
 SIZE_PLOTS = (25, 12.5)
 #(25, 12.5)
+#(50, 25)
 SIZE_LINE = 0.8
-SIZE_LABELS = 12
-BOX_PLACE = (0.01, 0.05)
-LOC_PLACE = "lower left"
-#"upper left"
-#(0.01, 0.95)
+SIZE_LABELS = 25
+BOX_PLACE = (0.01, 0.95)
+LOC_PLACE = "upper left"
+#En haut à gauche: "upper left" et (0.01, 0.95)
+#En bas à gauche: "lower left" et (0.01, 0.05)
+
+
 
 #m, μ, n, p
 UNIT_X = "m"
 UNIT_Y = 1
 
 units = ""
-
+INTEGRAL = False
 # Choose experience
-SELECTED_CHIPS = ["ml4may02"]
+SELECTED_CHIPS = ["ml4apr01"]
 SELECTED_EXPERIENCES = []
-SELECTED_GEOMETRIES =  ["200", "100"]
+SELECTED_GEOMETRIES =  ["150"]
 SELECTED_PLACEMENTS = []
 
 # --> moyenne ?
@@ -34,7 +45,7 @@ SELECTED_PLACEMENTS = []
 # Choose graphes ! several at once
 #GRAPHES_TO_PLOT = ["P-V 1V_2#1","P-V 2V_2#1","P-V 3V_2#1", "P-V 4V_2#1","P-V 5V_1#1", "PUND 5V_1#1", "P-V 7V_1#1", "P-V 10V_1#1",
 #                   "PUND 7V_1#1","PUND 10V_1#1", "IV 3V_1#1", "CV 3V_1#1", "IV 5V_1#1", "CV 5V_1#1"]
-GRAPHES_TO_PLOT = ["P-V 10V_for10V PUND#1", "P-V 10V_1#1"]
+GRAPHES_TO_PLOT = ["P-V 15V_1#1"]
 GRAPH_VOLTAGES = ["1"] # currently not used just enter the complet graph name above
 
 SHOW_PLOTS = True # put False if you don't want to show all plots during plot generation
@@ -44,11 +55,19 @@ RESULTS_TO_PLOT = []
 RESULTS_X_AXIS = "parameter x"
 RESULTS_LABELS = ""
 
-SPECIAL = True
-
-SPECIAL_CHIPS = ["ml4may02", "ml4may02"]
+SPECIAL = False
 SPECIAL_EXPERIENCES = []
-SPECIAL_GEOMETRIES = ["200", "100"]
-SPECIAL_PLACEMENT = []
+SPECIAL_GEOMETRIES = ["200"]
+SPECIAL_PLACEMENT = ["s1-a1"]
 
-SPECIAL_PLOT = ["P-V 10V_for10V PUND#1", "P-V 10V_1#1"]
+SPECIAL_PLOT = ["P-V 5V_1#1", "P-V 7V_1#1", "P-V 8V_1#1", "P-V 10V_1#1", "P-V 12V_1#1", "P-V 14V_1#1", "P-V 15V_1#1", 
+                  "P-V 17V_1#1", "P-V 18V_1#1","P-V 19V_1#1", "P-V 20V_1#1", "P-V 21V_1#1", "P-V 22V_1#1", "P-V 23V_1#1", 
+                  "P-V 24V_1#1", "P-V 25V_1#1", "P-V 28V_1#1", "P-V 29V_1#1", "P-V 30V_1#1","P-V 32V_1#1"]
+SPECIAL_CHIPS =  ["ml4apr06"] * len(SPECIAL_PLOT)
+
+#Need same length * len(SPECIAL_PLOT)
+# "ml4apr01",
+#"P-V 7V_1#1",
+
+PLOT_RESULT_ENERGY = True
+CHIP_RESULT_ENERGY = ["ml4may07"]
